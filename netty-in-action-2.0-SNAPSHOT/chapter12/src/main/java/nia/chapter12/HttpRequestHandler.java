f157cb1e532ce9cf75439a2b377a7185bc82d85a
xiaolj <xiaolj@jiguang.cn>
2021-11-04 23:27:43 +0800

    1

--- netty-in-action-2.0-SNAPSHOT/chapter12/src/main/java/nia/chapter12/HttpRequestHandler.java
+++ netty-in-action-2.0-SNAPSHOT/chapter12/src/main/java/nia/chapter12/HttpRequestHandler.java
@@ -20,16 +20,13 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
     private static final File INDEX;
 
     static {
-        URL location = HttpRequestHandler.class
-             .getProtectionDomain()
-             .getCodeSource().getLocation();
+        URL location = HttpRequestHandler.class.getProtectionDomain().getCodeSource().getLocation();
         try {
             String path = location.toURI() + "index.html";
             path = !path.contains("file:") ? path : path.substring(5);
             INDEX = new File(path);
         } catch (URISyntaxException e) {
-            throw new IllegalStateException(
-                 "Unable to locate index.html", e);
+            throw new IllegalStateException("Unable to locate index.html", e);
         }
     }
 
@@ -38,8 +35,7 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
     }
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx,
-        FullHttpRequest request) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
         if (wsUri.equalsIgnoreCase(request.getUri())) {
             ctx.fireChannelRead(request.retain());
         } else {
@@ -47,27 +43,20 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
                 send100Continue(ctx);
             }
             RandomAccessFile file = new RandomAccessFile(INDEX, "r");
-            HttpResponse response = new DefaultHttpResponse(
-                request.getProtocolVersion(), HttpResponseStatus.OK);
-            response.headers().set(
-                HttpHeaders.Names.CONTENT_TYPE,
-                "text/html; charset=UTF-8");
+            HttpResponse response = new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
             boolean keepAlive = HttpHeaders.isKeepAlive(request);
             if (keepAlive) {
-                response.headers().set(
-                    HttpHeaders.Names.CONTENT_LENGTH, file.length());
-                response.headers().set( HttpHeaders.Names.CONNECTION,
-                    HttpHeaders.Values.KEEP_ALIVE);
+                response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+                response.headers().set( HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
             }
             ctx.write(response);
             if (ctx.pipeline().get(SslHandler.class) == null) {
-                ctx.write(new DefaultFileRegion(
-                    file.getChannel(), 0, file.length()));
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
             } else {
                 ctx.write(new ChunkedNioFile(file.getChannel()));
             }
-            ChannelFuture future = ctx.writeAndFlush(
-                LastHttpContent.EMPTY_LAST_CONTENT);
+            ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
             if (!keepAlive) {
                 future.addListener(ChannelFutureListener.CLOSE);
             }
@@ -75,13 +64,12 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
     }
 
     private static void send100Continue(ChannelHandlerContext ctx) {
-        FullHttpResponse response = new DefaultFullHttpResponse(
-            HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
         ctx.writeAndFlush(response);
     }
+
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-        throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
     }
--- netty-in-action-2.0-SNAPSHOT/chapter12/src/main/java/nia/chapter12/TextWebSocketFrameHandler.java
+++ netty-in-action-2.0-SNAPSHOT/chapter12/src/main/java/nia/chapter12/TextWebSocketFrameHandler.java
@@ -11,8 +11,8 @@ import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
-public class TextWebSocketFrameHandler
-    extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
     private final ChannelGroup group;
 
     public TextWebSocketFrameHandler(ChannelGroup group) {
@@ -20,13 +20,10 @@ public class TextWebSocketFrameHandler
     }
 
     @Override
-    public void userEventTriggered(ChannelHandlerContext ctx,
-        Object evt) throws Exception {
-        if (evt == WebSocketServerProtocolHandler
-             .ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt == WebSocketServerProtocolHandler.ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
             ctx.pipeline().remove(HttpRequestHandler.class);
-            group.writeAndFlush(new TextWebSocketFrame(
-                    "Client " + ctx.channel() + " joined"));
+            group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + " joined"));
             group.add(ctx.channel());
         } else {
             super.userEventTriggered(ctx, evt);
@@ -34,8 +31,7 @@ public class TextWebSocketFrameHandler
     }
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx,
-        TextWebSocketFrame msg) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
         group.writeAndFlush(msg.retain());
     }
 }
