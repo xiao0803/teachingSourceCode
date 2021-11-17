6917d086c3efa5f282c88ab3db7d4c105d3ca961
lijun.xiao <lijun.xiao@transsnet.com>
2021-11-17 18:52:31 +0800

    1

--- netty-guide/src/main/java/com/phei/netty/protocol/http/xml/codec/HttpXmlRequestDecoder.java
+++ netty-guide/src/main/java/com/phei/netty/protocol/http/xml/codec/HttpXmlRequestDecoder.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2018 Lilinfeng.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,6 +18,7 @@ package com.phei.netty.protocol.http.xml.codec;
 import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
 import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -31,38 +32,33 @@ import java.util.List;
 
 /**
  * @author Lilinfeng
- * @date 2014年3月1日
  * @version 1.0
+ * @date 2014年3月1日
  */
-public class HttpXmlRequestDecoder extends
-	AbstractHttpXmlDecoder<FullHttpRequest> {
+public class HttpXmlRequestDecoder extends AbstractHttpXmlDecoder<FullHttpRequest> {
 
     public HttpXmlRequestDecoder(Class<?> clazz) {
-	this(clazz, false);
+        this(clazz, false);
     }
 
     public HttpXmlRequestDecoder(Class<?> clazz, boolean isPrint) {
-	super(clazz, isPrint);
+        super(clazz, isPrint);
     }
 
     @Override
-    protected void decode(ChannelHandlerContext arg0, FullHttpRequest arg1,
-	    List<Object> arg2) throws Exception {
-	if (!arg1.getDecoderResult().isSuccess()) {
-	    sendError(arg0, BAD_REQUEST);
-	    return;
-	}
-	HttpXmlRequest request = new HttpXmlRequest(arg1, decode0(arg0,
-		arg1.content()));
-	arg2.add(request);
+    protected void decode(ChannelHandlerContext arg0, FullHttpRequest arg1, List<Object> arg2) throws Exception {
+        if (!arg1.getDecoderResult().isSuccess()) {
+            sendError(arg0, BAD_REQUEST);
+            return;
+        }
+        HttpXmlRequest request = new HttpXmlRequest(arg1, decode0(arg0, arg1.content()));
+        arg2.add(request);
     }
 
-    private static void sendError(ChannelHandlerContext ctx,
-	    HttpResponseStatus status) {
-	FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
-		status, Unpooled.copiedBuffer("Failure: " + status.toString()
-			+ "\r\n", CharsetUtil.UTF_8));
-	response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
-	ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
+                status, Unpooled.copiedBuffer("Failure: " + status.toString() + "\r\n", CharsetUtil.UTF_8));
+        response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
 }
