308bf0bf869840ac96500869ccafe5118d988123
lijun.xiao <lijun.xiao@transsnet.com>
2022-07-11 17:59:25 +0800

    update

--- kafka_book_demo-master/src/main/java/chapter10/KafkaConsumerGroupAnother.java
+++ kafka_book_demo-master/src/main/java/chapter10/KafkaConsumerGroupAnother.java
@@ -15,43 +15,33 @@ import java.util.Optional;
 
 /**
  * 代码清单10-3
- * Created by 朱小厮 on 2018/10/22.
+ *
  */
 public class KafkaConsumerGroupAnother {
 
     public static void main(String[] args) throws IOException {
-        String[] agrs = {"--describe", "--bootstrap-server",
-                "localhost:9092", "--group", "groupIdMonitor"};
-        ConsumerGroupCommand.ConsumerGroupCommandOptions options =
-                new ConsumerGroupCommand.ConsumerGroupCommandOptions(agrs);
-        ConsumerGroupCommand.ConsumerGroupService kafkaConsumerGroupService =
-                new ConsumerGroupCommand.ConsumerGroupService(options);
+        String[] agrs = {"--describe", "--bootstrap-server", "localhost:9092", "--group", "groupIdMonitor"};
+        ConsumerGroupCommand.ConsumerGroupCommandOptions options = new ConsumerGroupCommand.ConsumerGroupCommandOptions(agrs);
+        ConsumerGroupCommand.ConsumerGroupService kafkaConsumerGroupService = new ConsumerGroupCommand.ConsumerGroupService(options);
 
         ObjectMapper mapper = new ObjectMapper();
         //1. 使用jackson-module-scala_2.11
         mapper.registerModule(new DefaultScalaModule());
         //2. 反序列化时忽略对象不存在的属性
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-                false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         //3. 将Scala对象序列化成JSON字符串
         //这里原本会有权限问题，通过序列化绕过
-        String source = mapper.writeValueAsString(kafkaConsumerGroupService.
-                collectGroupOffsets()._2.get());
+        String source = mapper.writeValueAsString(kafkaConsumerGroupService.collectGroupOffsets()._2.get());
         //4. 将JSON字符串反序列化成Java对象
-        List<PartitionAssignmentStateAnother> target = mapper.readValue(source,
-                getCollectionType(mapper,List.class,
-                        PartitionAssignmentStateAnother.class));
+        List<PartitionAssignmentStateAnother> target = mapper.readValue(source, getCollectionType(mapper,List.class, PartitionAssignmentStateAnother.class));
         //5. 排序
         target.sort((o1, o2) -> o1.getPartition() - o2.getPartition());
         //6. 打印
         printPasList(target);
     }
 
-    public static JavaType getCollectionType(ObjectMapper mapper,
-                                             Class<?> collectionClass,
-                                             Class<?>... elementClasses) {
-        return mapper.getTypeFactory()
-                .constructParametricType(collectionClass, elementClasses);
+    public static JavaType getCollectionType(ObjectMapper mapper, Class<?> collectionClass, Class<?>... elementClasses) {
+        return mapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
     }
 
     public static void printPasList(List<PartitionAssignmentStateAnother> list) {
